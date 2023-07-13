8c6ecba07908ce1cca5fb64300054134bf4bd710
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-13 09:25:33 +0530

    Add project files.

---
+++ EnumarableProject/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumarableProject
+{
+    public class Player
+    {
+        private int run;
+        private string name;
+
+        public Player(int run, string name)
+        {
+            this.run = run;
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return $"{run} - {name}";
+        }
+    }
+
+    public class Team : IEnumerable
+    {
+        Player[] players;
+        public Team()
+        {
+
+            players = new Player[3];
+            players[0] = new Player(1000, "Sachin");
+            players[1] = new Player(1100, "Virat");
+            players[2] = new Player(1400, "Dhoni");
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return players.GetEnumerator();
+        }
+    }
+}
---
+++ EnumarableProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumarableProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team india = new Team();
+
+            foreach (Player p in india)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
